032f9cc3889dd3b1e1e1f4a29a74f3729bd89fd9
Daniel Sampaio <danielsampaio127@gmail.com>
2025-01-04 11:49:48 -0300

    refactor: atualizando endereço da url para pagina de votacao de usuarios

--- src/app/enqueteAdmin/[id]/page.tsx
+++ src/app/enqueteAdmin/[id]/page.tsx
@@ -73,7 +73,7 @@ export default function EnquetePage() {
                         </div>
                         {qrCodeUrl && (
                             <div className="flex flex-col items-center justify-center w-full m-auto py-2">
-                                <Link className="bg-blue-700 text-zinc-300 rounded-md px-3 py-2 m-2" href={`https://avaliacao-de-satisfacao-lovat.vercel.app/enquete/${enqueteID.id}`}>Link QR Code</Link>
+                                <Link className="bg-blue-700 text-zinc-300 rounded-md px-3 py-2 m-2" href={`https://avaliacao-de-satisfacao-two.vercel.app/enquete/${enqueteID.id}`}>Link QR Code</Link>
                                 <a
                                     href={qrCodeUrl}
                                     download="qrcode.png"
